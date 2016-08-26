103eb725615e0c650cd0c40ae36ba02905735d47
garethmullins <gareth.mullins@my.jcu.edu.au>
2016-08-26 17:40:00 +1000

    Add rename months to num_of_months and refactor the 6 report printing lines into a function

--- cumulativeTotals.py
+++ cumulativeTotals.py
@@ -6,15 +6,19 @@ Starter code for cumulative total income program
 
 def main():
     incomes = []
-    months = int(input("How many months? "))
+    num_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, num_of_months + 1):
+        income = float(input("Enter income for month {}: ".format(str(month))))
         incomes.append(income)
 
+    income_report(incomes, num_of_months)
+
+
+def income_report(incomes, num_of_months):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, num_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
